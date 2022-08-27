7c2c0606b36b48c5f061d4a5a024931785ed4e7c
Rodolfo <rg.gonzalezmorales@gmail.com>
2022-08-27 01:43:53 -0400

    ADD -> Test Get all books in FirstFragment.

--- app/src/main/java/com/certification/libreriaecos/FirstFragment.kt
+++ app/src/main/java/com/certification/libreriaecos/FirstFragment.kt
@@ -1,17 +1,25 @@
 package com.certification.libreriaecos
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.certification.libreriaecos.databinding.FragmentFirstBinding
+import com.certification.libreriaecos.viewmodel.BookViewModel
+import com.certification.libreriaecos.viewmodel.BookViewModelFactory
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
+
+    private val bookViewModel: BookViewModel by activityViewModels {
+        BookViewModelFactory((activity?.application as AppBook).repository)
+    }
 
     private var _binding: FragmentFirstBinding? = null
 
@@ -32,8 +40,16 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        obserbData()
+
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    private fun obserbData() {
+        bookViewModel.getAllBooksFromDB().observe(viewLifecycleOwner){
+            Log.d("RODO", "UI observer $it")
         }
     }
 
