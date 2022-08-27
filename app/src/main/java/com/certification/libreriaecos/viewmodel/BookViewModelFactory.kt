2755796c50f93d23a6079730dd45897bf613e78d
Rodolfo <rg.gonzalezmorales@gmail.com>
2022-08-27 01:31:40 -0400

    ADD -> ViewModel Factory

---
+++ app/src/main/java/com/certification/libreriaecos/viewmodel/BookViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.certification.libreriaecos.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.certification.libreriaecos.data.BookRepository
+
+class BookViewModelFactory(private val repository: BookRepository) : ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T{
+        if (modelClass.isAssignableFrom(BookViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return BookViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
